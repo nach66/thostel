3fb7be459fea5b5b61d8bd65c935a73dc88dc3f8
unknown <nach666@gmail.com>
2020-08-23 17:42:55 +0300

    mini iframe

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -26,7 +26,7 @@ export default class minihotel extends Component {
                                 <h6>tiberiashostel@gmail.com</h6>
                             </article>
                             <br/>
-                            <a rel="noopener noreferrer" target="_blank" className="book-now-btn"
+  {/*                           <a rel="noopener noreferrer" target="_blank" className="book-now-btn"
                                 href={
 "https://minihotelpms.net/frame/reservas/getAvailability?hotelToken=a41ce7814d16061ac9a951e3bb1c534e&nAdults=1&nChilds=0&nBabies=0&language=HE&rateCode=*ALL&roomId=&from=2020-08-22&to=2020-08-23&minToDate=2020-08-23&nochesPedidas=1&bloggerId=&languageDirection=rtl"
                                 }
@@ -35,7 +35,15 @@ export default class minihotel extends Component {
                                     <span className="calander-icon">
                                         <FaCalendarAlt/>
                                     </span>
-                            </a>
+                            </a> */}
+
+
+                            <iframe id="be_iframe" frameborder="0" scrolling="no" 
+                                    style={{width: '100%', height: '172px', minHeight: '500px', overflow: 'hidden'}} 
+                                    onload="window.scrollTo(0,0)" 
+                                    src="https://minihotelpms.net/frame/reservas?hotelToken=a41ce7814d16061ac9a951e3bb1c534e||from=||to=||nAdults=1||nChilds=0||nBabies=0||start_date=||end_date=||language=HE||currency=ILS||rateCode=*ALL||roomId=||bloggerId=">
+                            </iframe>
+
 
                         </section>
                     <div className="sep"/>
